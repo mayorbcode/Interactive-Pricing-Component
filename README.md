b07296aa98224dc0de6bf4ae6488f8ed9d5fffd7
Patrick Bello <mayorbello@yahoo.com>
2021-02-20 13:04:22 -0700

    added styling for progress bar

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ by [Patrick Bello](https://github.com/mayorbcode)
 
 ## Objective
 Try to replicate [this](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8) design obtained from [Frontend Mentor](https://www.frontendmentor.io/challenges) using [React](https://reactjs.org/).
+
+## To-do
+- Make Responsive
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,36 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import Switch from './Switch';
 import { prices, pageViews } from '../../SubscriptionInfo';
 
 const Box = () => {
   const [ slider, setSlider ] = useState('2');
-  const [ checked, setChecked ] = useState(false)
-
-  const handleSlider = (e) => {
-    console.log(e.target.value);
+  const [ checked, setChecked ] = useState(false);
+  const [ covered, setCovered ] = useState(50);
+  
+  // Usecallback implementation as advised by Keanu
+  const handleSlider = useCallback((e) => {
+    // console.log(e.target.value);
     setSlider(e.target.value);
-  }
+  }, []);
 
   const handleChecked = (e) => {
-    console.log(e.target.checked);
+    // console.log(e.target.checked);
     setChecked(e.target.checked);
-  }
+  };
 
-  let covered;
-  let notCovered;
-  const testColor = (x) => {
-    covered = (x/4) * 100;
-    notCovered = 100 - covered;
-  }
+  // Usememo implementation as advised by Keanu
+  const checkPrice = useMemo(() => {
+    console.log('i ran')
+    return !checked ? prices[slider].toFixed(2) : (prices[slider] * 0.75).toFixed(2)
+  }, [slider, checked]);
 
+
+  useEffect(() => {
+    setCovered((slider/4) * 100)
+  }, [slider]);
+
+  console.log(covered);
+  
   return (
     <div className="box">
       <div className="top">
         <div className="heading">
           <p className="page-views">${pageViews[slider]} PAGEVIEWS</p>
-          <p><span>${!checked ? prices[slider].toFixed(2) : (prices[slider] * 0.75).toFixed(2)}</span> / month</p>
+          <p><span>${checkPrice}</span> / month</p>
         </div>
         <input 
+          style={{ background: `linear-gradient(to right, hsl(174, 77%, 80%) ${covered}%, hsl(224, 65%, 95%) 0%)` }}
           className="slider" 
           type="range"
           min="0"
